966b8898cbd46c456342b15aaffe2337de985c1f
Vishal-yolo <vishal.patel@yolohealth.in>
2021-12-29 00:17:59 +0530

    added new year counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from 'react'
 
-function App() {
+
+const App = () => {
+
+  const [days, setDays] = useState(10)
+  const [hours, setHours] = useState(10)
+  const [minutes, setMinutes] = useState(10)
+  const [seconds, setSeconds] = useState(10)
+  const [isLoading, setIsLoading] = useState(true)
+
+
+  const countDown = () => {
+    const endDate = new Date("December 31, 2021 00:00:00").getTime()
+    const today = new Date().getTime()
+
+    const timeDiff = endDate - today
+
+    const seconds = 1000
+    const minutes = seconds * 60
+    const hours = minutes * 60
+    const days = hours * 24
+
+    let timeDays = Math.floor((timeDiff / days))
+    let timeHours = Math.floor((timeDiff % days) / hours)
+    let timeMinutes = Math.floor((timeDiff % hours) / minutes)
+    let timeSeconds = Math.floor((timeDiff % minutes) / seconds)
+
+    timeHours = timeHours < 10 ? '0' + timeHours : timeHours
+    timeMinutes = timeMinutes < 10 ? '0' + timeMinutes : timeMinutes
+    timeSeconds = timeSeconds < 10 ? '0' + timeSeconds : timeSeconds
+
+    setDays(timeDays)
+    setHours(timeHours)
+    setMinutes(timeMinutes)
+    setSeconds(timeSeconds)
+    setIsLoading(false)
+  }
+
+
+  useEffect(() => {
+    setInterval(countDown, 1000)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <h1 className='container'>Loading...</h1>
+      ) : (
+        <section className='container'>
+          <h1> Countdown Timer for New Year</h1>
+
+          <div className='countdown'>
+            <article>
+              <p>{days}</p>
+              <h3>Days</h3>
+            </article>
+            <article>
+              <p>{hours}</p>
+              <h3>Hours</h3>
+            </article>
+            <article>
+              <p>{minutes}</p>
+              <h3>Minutes</h3>
+            </article>
+            <article>
+              <p>{seconds}</p>
+              <h3>Seconds</h3>
+            </article>
+          </div>
+        </section>
+      )
+      }
+    </>
   );
 }
 
